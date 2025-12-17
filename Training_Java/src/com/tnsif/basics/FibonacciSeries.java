7e4c678318d38f338157a2ee445c7b61ab621c58
divya <divya@DESKTOP-T8BK3SV>
2025-12-17 19:59:12 +0530

    commit2

---
+++ Training_Java/src/com/tnsif/basics/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package com.tnsif.basics;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int n,first=0,second=1,next;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of series:");
+		n=sc.nextInt();
+				System.out.println("Fibonacci Series:");
+				for(int i=1; i<=n;i++) {
+					System.out.println(first+"");
+					next = first + second ;
+					first =second;
+					second = next;
+				}
+		
+
+	}
+
+}
---
+++ Training_Java/src/com/tnsif/oops/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.oops.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Divya");
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+
+}
